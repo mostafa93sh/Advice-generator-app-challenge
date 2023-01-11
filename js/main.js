b114a6822d76ce408266b397e5e92e4d3664f27a
mostafa93sh <mostafa.khalel.93@gmail.com>
2023-01-11 02:24:46 +0200

    add event to dice area

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let adviceElement = document.getElementsByClassName("advice-content").item(0);
 let adviceIdElement = document.getElementsByClassName("advice-number").item(0);
+let greenCircle = document.getElementsByClassName("green-circle").item(0);
 const getQuotes = async () => {
   const response = await fetch("https://api.adviceslip.com/advice");
   const data = await response.json();
@@ -8,9 +9,13 @@ const getQuotes = async () => {
   adviceIdElement.innerHTML = `advice #${data.slip.id}`;
 };
 
+greenCircle.addEventListener("click", () => {
+  getQuotes();
+});
+
 // const quote = getQuotes()
 //   .then((data) => data.slip)
 //   .catch((err) => console.log(err));
-const timeToFetch = setTimeout(async () => {
-  getQuotes();
-}, 3000);
+// const timeToFetch = setTimeout(async () => {
+
+// }, 3000);
